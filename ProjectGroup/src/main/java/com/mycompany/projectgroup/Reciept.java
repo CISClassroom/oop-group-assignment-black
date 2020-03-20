5fc64a62e3938eb7b774581c9bb8d280dd80df36
kwannattha <58881048+kwannattha@users.noreply.github.com>
2020-03-20 07:12:46 +0700

    Finish

---
+++ ProjectGroup/src/main/java/com/mycompany/projectgroup/Reciept.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectgroup;
+
+/**
+ *
+ * @author KWANNATTHA
+ */
+class Reciept {
+
+    static char[] getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
